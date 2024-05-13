79ef2d94b4612c691bc5d780d3171c2bd44ec588
SaccoUnibg <c.sacco@studenti.unibg.it>
2024-05-13 14:07:53 +0200

    first commit

---
+++ src/main/java/com/unibg/UnibgProjectFrontend/UnibgProjectFeApplication.java
@@ -0,0 +1,13 @@
+package com.unibg.UnibgProjectFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibgProjectFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibgProjectFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unibg/UnibgProjectFrontend/UnibgProjectFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibg.UnibgProjectFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibgProjectFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
